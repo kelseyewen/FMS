d52ba959d4d12d3ebb7280990898a6e7fd6b2e54
Ewen <kelsey.ewen@agr.gc.ca>
2023-02-10 15:35:00 -0500

    Responses for SMS5

--- SMS5.R
+++ SMS5.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(tidyr)
 
 # Count the responses to the multiple choice question
-count_responses <- Poultry %>%
+count_responses <- Dairy %>%
   group_by(PROV, SMS5) %>%
   summarize(count = n())
 
@@ -11,14 +11,22 @@ count_responses <- Poultry %>%
 count_responses <- count_responses[order(count_responses$PROV, count_responses$SMS5), ]
 
 # Create a pivot table to show the results organized by province and answer chosen
-Poultry_SMS5 <- count_responses %>%
+Dairy_SMS5 <- count_responses %>%
   spread(SMS5, count)
+print(Dairy_SMS5)
 
 #change column names and remove column that shows the number of blank answers
-colnames(Poultry_SMS5) <- c("Province", "<6 months", "6-12 months", "1-2 Years", ">2 Years")
-Poultry_SMS5 <- Poultry_SMS5[,-6]
+colnames(Dairy_SMS5) <- c("Province", "<6 months", "6-12 months", "1-2 Years")
+Dairy_SMS5 <- Dairy_SMS5[,-5]
 
 #Save as a data frame
-Poultry_SMS5 <- as.data.frame(Poultry_SMS5)
+Dairy_SMS5 <- as.data.frame(Dairy_SMS5)
 
-View(Poultry)
+View(Dairy_SMS5)
+
+#Export tables to excel
+xls.list <- list(Dairy_SMS5=Dairy_SMS5)
+for (i in 1:1) {
+  write.xlsx(as.data.frame(Dairy_SMS5),
+             file = "results/Dairy.xlsx",
+             sheetName = names(xls.list)[1], row.names = F, col.names=TRUE, append = TRUE)}
